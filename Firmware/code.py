76eaf244caf2c9f033824ea199cb312e7bc27692
tatsu22 <millertyl0616@gmail.com>
2022-06-11 15:55:59 -0400

    updates

--- Firmware/code.py
+++ Firmware/code.py
@@ -4,17 +4,21 @@ import board
 import busio
 import time
 import usb_hid
+# from adafruit_hid.keyboard import Keyboard
 from bitmap_keyboard import BitmapKeyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 #from pca950x import PCA950x
 from mirage import KeyGrid
 from keymapping import Keymap
+#from clicky_buttons import *
 from util import timed_function
 import parser
 
 print('MiRage keyboard go!')
 
 i2c = busio.I2C(board.SCL, board.SDA, frequency=576000)
+#i2c1 = busio.I2C(board.SCL, board.SDA, frequency=576000)
+# usb_keyboard = Keyboard(usb_hid.devices)
 usb_keyboard = BitmapKeyboard(usb_hid.devices)
 hid_keyboard_locale = KeyboardLayoutUS(usb_keyboard)
 
@@ -22,12 +26,21 @@ while not i2c.try_lock():
     print('Failed to lock I2C')
     time.sleep(0.5)
 
-print("I2C addresses found on bus:", [hex(device_address)
+#while not i2c1.try_lock():
+#    print('Failed to lock I2C1')
+#    time.sleep(0.5)
+
+print("I2C addresses found on bus 0:", [hex(device_address)
               for device_address in i2c.scan()])
 
+#print("I2C addresses found on bus 1:", [hex(device_address)
+#              for device_address in i2c1.scan()])
+
 i2c.unlock()
+#i2c1.unlock()
 
 try:
+    # parser.debug_line_callback = cool_ass_parsing_visualizer
 
     keymap = Keymap(usb_keyboard, hid_keyboard_locale)
     keymap.load('/MiRage Keymaps')  # TODO: Detect and automatically switch to Rage pad
@@ -60,4 +73,5 @@ try:
 finally:
     print('it ded')
     i2c.unlock()
+#    i2c1.unlock()
     usb_keyboard.release_all()
--- Firmware/mirage.py
+++ Firmware/mirage.py
@@ -1,70 +1,90 @@
 import time
 from pca950x import PCA950x
-from mcp23018 import MCP2301x
+#from mcp23018 import MCP2301x
 from util import timed_function
 
 # TODO: remap these to follow my schem
 physical_key_assignments = {
-
+    (1, 3, 7): (0, 0),
+    (1, 4, 0): (0, 1),
+    (1, 4, 1): (0, 2),
+    (1, 4, 2): (0, 3),
+    (1, 4, 3): (0, 4),
+    (1, 4, 4): (0, 5),
+    (1, 4, 5): (0, 6),
+    (1, 3, 0): (1, 0),
+    (1, 3, 1): (1, 1),
+    (1, 3, 2): (1, 2),
+    (1, 3, 6): (1, 3),
+    (1, 3, 5): (1, 4),
+    (1, 3, 4): (1, 5),
+    (1, 3, 3): (1, 6),
+    (1, 2, 7): (2, 0),
+    (1, 2, 6): (2, 1),
+    (1, 2, 5): (2, 2),
+    (1, 2, 4): (2, 3),
+    (1, 2, 1): (2, 4),
+    (1, 2, 2): (2, 5),
+    (1, 2, 3): (2, 6),
+    (1, 2, 0): (3, 0),
+    (1, 1, 7): (3, 1),
+    (1, 1, 6): (3, 2),
+    (1, 1, 5): (3, 3),
+    (1, 1, 4): (3, 4),
+    (1, 1, 3): (3, 5),
+    (1, 1, 2): (3, 6),
+    (1, 1, 1): (4, 0),
+    (1, 1, 0): (4, 1),
+    (1, 0, 7): (4, 2),
+    (1, 0, 6): (4, 3),
+    (1, 0, 5): (4, 4),
+    (1, 0, 4): (4, 5),
+    (1, 0, 2): (5, 0),
+    (1, 0, 3): (5, 1),
+    (1, 0, 0): (5, 2),
+    (1, 0, 1): (5, 3),
+    (0, 0, 2): (0, 7),
+    (0, 0, 3): (0, 8),
+    (0, 0, 4): (0, 9),
+    (0, 0, 5): (0, 10),
+    (0, 0, 6): (0, 11),
+    (0, 0, 7): (0, 12),
+    (0, 1, 0): (0, 13),
+    (0, 1, 1): (1, 7),
+    (0, 1, 2): (1, 8),
+    (0, 1, 3): (1, 9),
+    (0, 1, 4): (1, 10),
+    (0, 1, 5): (1, 11),
+    (0, 1, 6): (1, 12),
+    (0, 1, 7): (1, 13),
+    (0, 3, 6): (2, 7),
+    (0, 3, 7): (2, 8),
+    (0, 4, 0): (2, 9),
+    (0, 2, 3): (2, 10),
+    (0, 2, 2): (2, 11),
+    (0, 2, 1): (2, 12),
+    (0, 2, 0): (2, 13),
+    (0, 3, 2): (3, 7),
+    (0, 3, 1): (3, 8),
+    (0, 3, 0): (3, 9),
+    (0, 2, 7): (3, 10),
+    (0, 2, 6): (3, 11),
+    (0, 2, 5): (3, 12),
+    (0, 2, 4): (3, 13),
+    (0, 4, 3): (4, 6),
+    (0, 4, 2): (4, 7),
+    (0, 4 ,1): (4, 8),
+    (0, 3, 5): (4, 9),
+    (0, 3, 4): (4, 10),
+    (0, 3, 3): (4, 11),
+    (0, 4, 6): (5, 4),
+    (0, 4, 7): (5, 5),
+    (0, 4, 4): (5, 6),
+    (0, 4, 5): (5, 7)
 
 # EXAMPLE
-#    (0, 0, 5): (0, 1),  #  (which PCA9505, which bank, which line): (row, col)
+#  (which PCA9505, which bank, which line): (row, col)
 #    (0, 0, 6): (0, 2),
-#    (0, 0, 7): (0, 3),
-#    (0, 1, 0): (2, 4),
-#    (0, 1, 1): (1, 4),
-#    (0, 1, 2): (0, 4),
-#    (0, 1, 3): (3, 5),
-#    (0, 1, 4): (2, 5),
-#    (0, 1, 5): (1, 5),
-#    (0, 1, 6): (0, 5),
-#    (0, 1, 7): (4, 4),
-#    (0, 2, 0): (3, 6),
-#    (0, 2, 1): (2, 6),
-#    (0, 2, 2): (1, 6),
-#    (0, 2, 3): (0, 6),
-#    (0, 2, 5): (3, 4),
-#    (0, 2, 6): (4, 3),
-#    (0, 2, 7): (4, 2),
-#    (0, 3, 0): (1, 3),
-#    (0, 3, 1): (2, 3),
-#    (0, 3, 2): (3, 3),
-#    (0, 3, 4): (1, 2),
-#    (0, 3, 5): (2, 2),
-#    (0, 3, 6): (3, 2),
-#    (0, 3, 7): (4, 1),
-#    (0, 4, 0): (1, 1),
-#    (0, 4, 1): (2, 1),
-#    (0, 4, 2): (0, 0),
-#    (0, 4, 3): (3, 1),
-#    (0, 4, 4): (1, 0),
-#    (0, 4, 5): (2, 0),
-#    (0, 4, 6): (3, 0),
-#    (0, 4, 7): (4, 0),
-#    (1, 0, 0): (1, 11),
-#    (1, 0, 1): (1, 10),
-#    (1, 0, 2): (1, 9),
-#    (1, 0, 3): (1, 8),
-#    (1, 0, 4): (1, 7),
-#    (1, 0, 5): (2, 11),
-#    (1, 0, 6): (2, 10),
-#    (1, 0, 7): (2, 9),
-#    (1, 1, 0): (2, 8),
-#    (1, 1, 1): (2, 7),
-#    (1, 1, 2): (3, 11),
-#    (1, 1, 3): (4, 8),
-#    (1, 1, 5): (4, 7),
-#    (1, 1, 6): (4, 6),
-#    (1, 1, 7): (4, 5),
-#    (1, 2, 0): (3, 7),
-#    (1, 2, 1): (3, 8),
-#    (1, 2, 2): (3, 9),
-#    (1, 2, 3): (3, 10),
-#    (1, 3, 1): (0, 7),
-#    (1, 3, 2): (0, 8),
-#    (1, 3, 3): (0, 9),
-#    (1, 3, 4): (0, 10),
-#    (1, 3, 5): (0, 11)
 }
 
 class IOAssociation:
@@ -93,8 +113,8 @@ class KeyGrid:
         self.double_click_timeout = 0.2  # Click again within this many seconds to fire a double click
 
     def setup(self):
-        #self.io_expanders.append(PCA950x(self.i2c, 0x23)) # TODO: Uncomment when we make the second half
-        self.io_expanders.append(PCA950x(self.i2c, 0x20))
+        self.io_expanders.append(PCA950x(self.i2c, 0x20)) # TODO: Uncomment when we make the first half
+        self.io_expanders.append(PCA950x(self.i2c, 0x24)) # TODO: Uncomment when we make the second half
         #self.io_expanders.append(MCP2301x(self.i2c, 0x20))
 
         for io_expander in self.io_expanders:
@@ -107,6 +127,9 @@ class KeyGrid:
                 if coords in self.keys:
                     row, col = self.keys[coords]
                     is_a_key = True
+#                elif coords in self.buttons:
+#                    row, col = self.buttons[coords]
+#                    is_a_key = False
                 else:
                     continue
 
@@ -121,6 +144,9 @@ class KeyGrid:
             if line.input_line.fell:  # Key pressed down
                 print('Row {}, col {} pressed'.format(line.row, line.col))
                 self.keymap.fire_operation(line.row, line.col, 'press')
+#                if self.clicky_displays is not None:
+#                    for display in self.clicky_displays:
+#                        display.on_keystroke(0x00)  # TODO: Only do something if a key was pressed, and pass in which key
 
                 self.key_down_timestamps[line] = time.monotonic()
 
@@ -148,6 +174,8 @@ class KeyGrid:
                             is_double_click = True
                     else:
                         self.recent_key_clicks[line] = time.monotonic()
+
+                    # TODO: When both a click and double click are assigned, clicking once should delay til DC threshold passed
 
                     if is_double_click:
                         # print("Row {}, col {} double-clicked".format(line.row, line.col))
